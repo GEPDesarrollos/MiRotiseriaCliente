cc9dde0db9653e12c01190f8ee2110b33fc50e6c
Guillermo Penedo <gepdesarrollodeapss@gmail.com>
2017-10-01 17:43:44 -0300

    Clase Cliente

---
+++ app/src/main/java/com/gep/desarrollos/mirotiseriacliente/Cliente.java
@@ -0,0 +1,29 @@
+package com.gep.desarrollos.mirotiseriacliente;
+
+/**
+ * Created by guille on 01/10/2017.
+ */
+
+class Cliente {
+
+    private String idCliente;
+    private String Observaciones;
+    // obtener la identidad de face
+
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getObservaciones() {
+        return Observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        Observaciones = observaciones;
+    }
+}
