0e0a887c227bb54ec6ce98337846f22de286510d
Guillermo Penedo <gepdesarrollodeapss@gmail.com>
2017-10-01 11:46:49 -0300

    Interface Controlador Cliente

--- app/src/main/java/com/gep/desarrollos/mirotiseriacliente/IControladorCliente.java
+++ app/src/main/java/com/gep/desarrollos/mirotiseriacliente/IControladorCliente.java
@@ -5,4 +5,45 @@ package com.gep.desarrollos.mirotiseriacliente;
  */
 
 public interface IControladorCliente {
+
+    /**
+     * metodo manejador para la acción del boton + en el plato
+     */
+    void manejadorBotonMas(String idPlato)throws ExcepcionRotiseria;
+
+    /**
+     * metodo manejador para la acción del boton - en el plato
+     */
+    void manejadorBotonMenos(String idPlato)throws ExcepcionRotiseria;
+
+    /**
+     * metodo manejador para la acción del boton OK en barraPedido. Pantalla1
+     */
+    void manejadorOkBarraPedido()throws ExcepcionRotiseria;
+
+
+    /**
+     * metodo manejador para la acción del boton agregar más platos. Pantalla2.
+     */
+    void manejadorBotonAgregarPlatos(String idPlato)throws ExcepcionRotiseria;
+
+    /**
+     * metodo manejador para la acción del boton OK enviar Pedido. Pantalla2.
+     */
+    void manejadorBotonEnviarPedido()throws ExcepcionRotiseria;
+
+
+    /**
+     * metodo manejador para la acción del boton Salir. Pantalla3.
+     */
+    void manejadorBotonSalir()throws ExcepcionRotiseria;
+
+    /**
+     * metodo manejador para la acción del boton comenzarTimer. Pantalla3.
+     */
+    void manejadorComenzarTimer()throws ExcepcionRotiseria;
+
+
+
+
 }
